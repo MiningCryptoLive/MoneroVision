c3202e57adbb9ac8fb3c9f02c2d40b88a965d1d8
MiningCryptoLive <celldr69@gmail.com>
2025-04-03 10:42:43 -0400

    Update index.tsx

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -12,85 +12,35 @@ export const Footer = () => (
         rel="noopener noreferrer"
         href="https://mycrypto.com"
       >
-        <PoweredByMyCrypto />
+        <PoweredBy />
       </a>
       <div className="flex-spacer" />
       <div className="Footer-main-content">
-        <a className="Footer-logo" href="https://monerovision.com/">
+        <a className="Footer-logo" href="https://explorer.mine-xmr.pro/">
           <Logo />
         </a>
         <p className="Footer-main-content-text">
-          MoneroVision is an open-source Monero block explorer that is offered as a part of the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://mycrypto.com">
-            MyCrypto
+          explorer.mine-xmr.pro is an open-source Monero block explorer that is offered as a part of the{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://explorer.mine-xmr.pro">
+            MINE-XMR.PRO
           </a>{' '}
-          network. At MyCrypto, we’re focused on building awesome products that put the power in
+          network. At MINE-XMR.PRO, we’re focused on building awesome products that put the power in
           your hands and this is just the beginning of our Monero offerings.
         </p>
       </div>
       <div className="flex-spacer" />
       <div className="Footer-social-media-wrapper">
-        <p className="Footer-social-titles">MoneroVision</p>
-        <div className="Footer-social-titles-wrapper">
+        <p className="Footer-social-titles">MINE-XMR.PRO</p>
           <a
             className="Footer-social-media-link"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://twitter.com/monerovision"
-          >
-            <i className="nc-icon nc-logo-twitter size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/MyCryptoHQ/MoneroVision"
+            href="https://github.com/MiningCryptoLive"
           >
             <i className="nc-icon nc-logo-github size_24px" />
           </a>
         </div>
-        <p className="Footer-social-titles">MyCrypto</p>
-        <div className="Footer-social-titles-wrapper">
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/mycrypto"
-          >
-            <i className="nc-icon nc-logo-twitter size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/MyCryptoHQ"
-          >
-            <i className="nc-icon nc-logo-github size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.instagram.com/mycryptohq"
-          >
-            <i className="nc-icon nc-logo-instagram size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.facebook.com/mycryptoHQ"
-          >
-            <i className="nc-icon nc-logo-facebook size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@mycrypto"
-          >
-            <i className="nc-icon nc-logo-medium size_24px" />
-          </a>
+       
         </div>
       </div>
     </div>
